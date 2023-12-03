b47864fcd4ba233b28b4536b9e65eded1b664b1e
josh <96921768+joshwakelin@users.noreply.github.com>
2023-12-02 21:57:03 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,26 +4,60 @@ network = {}
 
 pool = {}
 
+backbone.__Error(fatal, message) 
+   assert(fatal, message)
+   return message
+end
+
 backbone.CreateProtected()
 
 end
 
 
-backbone.Create()
+backbone.Create(id, data)
+   if network[id] then
+     return self.__Error(false, "Network ID already exists.")
+   else
+      network[id] = {
+         ['protected'] = false,
+         ['ref'] = data["name"] or nil,
+         ['functions'] = {},
+         ['blocks'] = data["blockType"] or false,
+         ['logs'] = {},
+         ['debounce'] = {},
+      
+      }
+      network[id]['logs'][1] = "Network created at ".. tostring(os.time()) .." via module creation."
 
 end
 
 backbone.Fire()
-
+   
 end
 
 backbone.ProtectedFire()
 
 end
 
-backbone.Sever()
+
+backbone.Sever(id)
+   
+end
+
+
+backbone.Disable(id)
+
 
 end
+
+
+backbone.Enable(id)
+   
+end
+
+backbone.EditFunction()
+
+end 
 
 backbone.Block()
 
