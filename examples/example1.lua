56efc818b867c702e115d3842ad6db267096926c
josh <96921768+joshwakelin@users.noreply.github.com>
2023-12-06 12:13:06 -0800

    Update example1.lua

---
+++ examples/example1.lua
@@ -0,0 +1,24 @@
+-- the following is an example of adding points to someones leaderstats using backbone.
+
+
+local function fufill() 
+
+
+end
+
+
+local function checkBlacklist()
+
+
+end
+
+
+local function set()
+   local connection;
+   connection = game.Players.PlayerAdded:Connect(function()
+      print(Player.Name.." added")
+   end
+
+
+
+end
